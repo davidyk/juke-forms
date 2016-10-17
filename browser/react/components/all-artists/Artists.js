09126856ac110f88fd21948122761d3ac96576af
David Y. Kim <davidykim@gmail.com>
2016-10-17 18:47:02 -0400

    got the first part

--- browser/react/components/all-artists/Artists.js
+++ browser/react/components/all-artists/Artists.js
@@ -5,13 +5,33 @@ import { Link } from 'react-router';
 
 export default function (props) {
   const artists = props.artists;
-  
+
+  console.log("artists", artists)
+  console.log("props.artistFilterName", props.artistFilterName)
+
+  var newArtists = artists.filter( function (artist){
+        console.log(artist.name, "artist.name")
+        console.log("expression", artist.name.search(props.artistFilterName))
+        if(artist.name.search(props.artistFilterName) === -1) {
+          return false;
+        }
+        return true;
+
+  });
+  console.log("newArtists", newArtists);
+
+  // var newArtists = artists.filter( artist =>
+  //       artist.search(props.artistFilterName) );
+  // console.log("newArtists", newArtists);
+
+
   return (
     <div>
       <h3>Artists</h3>
+      <input type="text" className="form-group" name="searchArtist" onChange={props.handleChange} />
       <div className="list-group">
         {
-          artists.map(artist => (
+          newArtists.map(artist => (
             <div className="list-group-item" key={ artist.id }>
               <Link to={`/artists/${artist.id}`}>
                 { artist.name }
--- browser/react/components/all-artists/ArtistsContainer.js
+++ browser/react/components/all-artists/ArtistsContainer.js
@@ -1,7 +1,53 @@
 'use strict';
-
+import React from 'react';
 import { connect } from 'react-redux';
 import Artists from './Artists';
+
+function ArtistsDecorator (InnerComponent) {
+
+  return class StatefulForm extends React.Component {
+
+
+    constructor (props) {
+      super(props);
+      this.state = {
+        artistFilterName: ""       //current entry from input
+      }
+
+      this.handleChange= this.handleChange.bind(this)
+    }
+
+    handleChange (evt) {
+      console.log(evt.target.value);
+      console.log("handleChange is running");
+
+      this.setState( {artistFilterName: evt.target.value});
+
+
+
+      // var newArtists = this.props.artists.filter( artist =>
+      //       artist.search(evt.target.value) );
+
+
+      console.log("artists", artists)
+
+
+  }
+
+    render() {
+      return (
+        <InnerComponent
+          artistFilterName={this.state.artistFilterName}
+          handleChange={this.handleChange}
+          artists={this.props.artists}
+          />
+      )
+    }
+  }
+}
+
+
+
 
 const mapStateToProps = function (state) {
   return {
@@ -10,5 +56,5 @@ const mapStateToProps = function (state) {
 };
 
 const statefulComponentCreator = connect(mapStateToProps);
-const ArtistsContainer = statefulComponentCreator(Artists);
+const ArtistsContainer = statefulComponentCreator(ArtistsDecorator(Artists));
 export default ArtistsContainer;
